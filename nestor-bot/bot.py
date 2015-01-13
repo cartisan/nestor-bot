3eac8d5d6d16812ccf2bcf8349866e60765de356
cartisan <leon@die-genossen.de>
2015-01-13 16:50:27 +0000

    initial bot structure loading a single actor and printing it

--- nestor-bot/bot.py
+++ nestor-bot/bot.py
@@ -1,5 +1,14 @@
 from cc_pattern.noc import noc
+from random import randint
 
+people_count = len(noc)
 
-print "number of nocs:"
-print len(noc)
+# choose random charcter
+person_number = randint(0, people_count-1)
+
+# mock the number for development
+person_number = 0
+
+actor = noc[person_number]
+print actor
+
