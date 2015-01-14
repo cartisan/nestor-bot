57c95b2b8f20428790a5deb4df3537fb040e5533
cartisan <leon@die-genossen.de>
2015-01-14 23:30:42 +0000

    Improved partners from group affiliation

--- nestor-bot/bot.py
+++ nestor-bot/bot.py
@@ -46,6 +46,30 @@ def choose_problem(actor):
     return problem
 
 
+def partners_from_group(actor):
+    prohibited_list = [actor.character[0]]
+    partners = []
+
+    #for each row
+    for r in NOC:
+        for v in r["Group Affiliation"]:
+            for actor_group in actor.group_affiliation:
+                if actor_group in v:
+                    partner = create_person_from_name(r["Character"])
+                    if not partner.character[0] in prohibited_list:
+                        partners.append(partner)
+                        prohibited_list.append(partner.character[0])
+
+    #TODO: Identify only the important ones
+    #right now only chance decision
+    amount = min(2, len(partners))
+    relevant_partners = []
+    for i in range(amount):
+        partner = choice(partners)
+        relevant_partners.append(partner)
+    return relevant_partners
+
+
 def calculate_partners(actor):
     # only use relations that actor does have
     possible_relations = RELATION_TYPES - actor.missing_attributes()
@@ -63,6 +87,10 @@ def calculate_partners(actor):
         if name in NAMES_INDEX.keys():
             partner = create_person_from_name(name)
             possible_partners.append(partner)
+
+    if actor.group_affiliation:
+        more_peeps = partners_from_group(actor)
+        possible_partners += more_peeps
 
     return possible_partners
 
@@ -95,13 +123,14 @@ def generate_solution_tweet(actor,partner,relationship,problem):
     #print problem
     pattern = TextPattern()
     problem_text = pattern.generate_solution_text(relationship,problem)
-    
+
     tmp = partner.character[0]
     prob = getattr(partner,problem)[0]
     actor_name = actor.character[0].replace(" ","")
-    
+
     return problem_text.format(tmp,actor_name,prob)
-    
+
+
 def generate_problem_solution(actor,partner,actor_problem,partner_problem):
     if(problem == "vehicle_of_choice"):
         sol_prob = realizevehicle(actor,partner,actor_problem,partner_problem)
@@ -133,7 +162,7 @@ print call_repo[0]
 print call_repo[1]
 
 # To go online, make it True!
-tweetme = True
+tweetme = False
 if tweetme:
     twitt = Tweeter()
     distressed_tweet = problem_list[1]
