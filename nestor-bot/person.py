98eb558e29a2ae17e11ce61c5b45857671653901
petham <hammarp@gmail.com>
2015-01-14 13:01:33 +0100

    person class

---
+++ nestor-bot/person.py
@@ -0,0 +1,16 @@
+class Person:
+    def __init__(self, d):
+        for key, value in d.items():
+            k2 = key.lower()
+            k3 = k2.replace(" ", "_")
+            # Input is a mix of strings, list, and lists of empty strings. Want lists of strings or empty lists.
+            newvalue = []
+            if type(value) == list:
+                for v in value:
+                    if v:
+                        newvalue.append(v)
+            elif type(value) == str:
+                newvalue.append(value)
+            else:
+                print "Value " + value + " is neither list nor string"
+            setattr(self, k3, newvalue)
