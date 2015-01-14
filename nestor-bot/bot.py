2299908c4713588b3acbe29016f0ad2736a45e44
cartisan <leon@die-genossen.de>
2015-01-14 11:41:26 +0000

    First bot version ready. Mostly fake it till we make it

--- nestor-bot/bot.py
+++ nestor-bot/bot.py
@@ -1,4 +1,4 @@
-from cc_pattern.noc import noc
+from cc_pattern.noc import noc as NOC
 from random import randint
 
 PROBLEM_TYPES = set(["Marital Status",
@@ -8,26 +8,69 @@ PROBLEM_TYPES = set(["Marital Status",
                      "Creator",
                      "Group Affiliation"])
 
-people_count = len(noc)
 
-# choose random charcter
-person_number = randint(0, people_count-1)
+def choose_actor():
+    people_count = len(NOC)
 
-actor = noc[person_number]
+    # choose random charcter
+    person_number = randint(0, people_count-1)
 
-# choose random problem
-# find missing entries:
-missing_properties = set()
-for k, v in actor.items():
-    if type(v) == list:
-        if not v[0]:
-            missing_properties.add(k)
-    else:
-        if not v:
-            missing_properties.add(k)
+    actor = NOC[person_number]
+    return actor
 
-# problems possible for our character
-possible_problems = list(PROBLEM_TYPES - missing_properties)
 
-problem = possible_problems[randint(0, len(possible_problems))-1]
-print problem
+# TODO: Repeat until one is found?
+def choose_problem(actor):
+    # choose random problem
+    # find missing entries:
+    missing_properties = set()
+    for k, v in actor.items():
+        if type(v) == list:
+            if not v[0]:
+                missing_properties.add(k)
+        else:
+            if not v:
+                missing_properties.add(k)
+
+    # problems possible for our character
+    possible_problems = list(PROBLEM_TYPES - missing_properties)
+
+    problem = possible_problems[randint(0, len(possible_problems))-1]
+    return problem
+
+
+def choose_partner(actor, problem):
+    people_count = len(NOC)
+    partner = None
+
+    #partner can't be the same as the actor
+    while partner is None or partner == actor:
+        # choose random charcter
+        person_number = randint(0, people_count-1)
+
+        partner = NOC[person_number]
+
+    return partner
+
+
+def generate_problem_tweet(actor, problem):
+    # call function from pattern module
+    return "{0}: I feel sad because of {1}.".format(actor["Character"],
+                                                    actor[problem])
+
+
+def generate_solution_tweet(partner, problem):
+    return "{0}: Cheer up!".format(partner["Character"])
+
+
+actor = choose_actor()
+problem = choose_problem(actor)
+#print problem
+problem_tweet = generate_problem_tweet(actor, problem)
+
+partner = choose_partner(actor, problem)
+solution_tweet = generate_solution_tweet(partner, problem)
+
+
+print problem_tweet
+print solution_tweet
